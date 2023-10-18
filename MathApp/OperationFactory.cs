155ffefe79be91887b1cd9d7930f878af7be7ea5
HukakHePak <valsidalvonilb@outlook.com>
2023-10-18 09:21:52 +0400

    feat: добавил операцию параметрическое отношение (2 - x) / (3 + y)

--- MathApp/OperationFactory.cs
+++ MathApp/OperationFactory.cs
@@ -15,6 +15,7 @@ namespace MathApp
             Operations = new List<OperationsEnum>();
             Operations.Add(new OperationsEnum() { Id = 0, Operation = new Operations.Add() });
             Operations.Add(new OperationsEnum() { Id = 1, Operation = new Operations.Sqrt() });
+            Operations.Add(new OperationsEnum() { Id = 3, Operation = new Operations.TwoMinusXPerThreePlusY() });
         }
         public static List<OperationsEnum> GetOperations()
         {
